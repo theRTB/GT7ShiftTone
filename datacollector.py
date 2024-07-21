7729698c602f3454368332d59d4efa53a34e38de
theRTB <wreckernl@gmail.com>
2024-07-21 10:42:11 +0200

    added linear interpolation for power/torque to highest rpm seen in accelrun, this is technically above revlimit

--- datacollector.py
+++ datacollector.py
@@ -11,10 +11,29 @@ from utility import rolling_avg, np_drag_fit
 #Given an array of consecutive rpm/accel points at full throttle and an array
 #of consecutive accel points with the clutch disengaged we can derive a torque
 #curve and thus a power curve.
+#TODO: Do we round revlimit? It is generally above true revlimit.
+#At stock, revlimit is a multiple of 100, but upgrades can be things like 3%
+#more revs and make it a random number. 
+#Appending a single value to an np.array is not efficient
 class PowerCurve():
-    def __init__(self, *args, **kwargs):
-        result = np_drag_fit(*args, **kwargs)
+    def __init__(self, accelrun, dragrun, *args, **kwargs):
+        result = np_drag_fit(accelrun, dragrun, *args, **kwargs)
+        self.revlimit = accelrun.revlimit
         self.rpm, self.torque, self.power = result
+
+        self.correct_final_point()
+
+    def correct_final_point(self):
+        x1, x2 = self.rpm[-2:]
+        print(f'x1 {x1:.3f} x2 {x2:.3f} revlimit {self.revlimit}')
+        np.append(self.rpm, self.revlimit)
+        self.rpm = np.append(self.rpm, self.revlimit)
+        for name in ['power', 'torque']: #,'boost']:
+            array = getattr(self, name)
+            y1, y2 = array[-2:]
+            ynew = (y2 - y1) / (x2 - x1) * (self.revlimit - x2) + y2
+            setattr(self, name, np.append(array, ynew))
+            print(f'y1 {y1:.3f} y2 {y2:.3f} ynew {ynew:.3f}')
 
     #get peak power according to peak power rounded to 0.1kW
     #the rounding is necessary to avoid some randomness in collecting a curve
